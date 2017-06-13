f48a8b0fb1a3b6bcc0af6fa1a43c0e0d8b00d7e0
Marilyn Carlin and Olena Kuchko <marilyn+olena@>
2017-06-13 16:27:28 -0700

    add functionality to add new department.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -14,7 +14,25 @@ namespace Registrar
         model.Add("listCourses", Course.GetAll());
         model.Add("listTeachers", Teacher.GetAll());
         model.Add("listStudents", Student.GetAll());
-        // model.Add("show-info", null);
+        model.Add("show-info", null);
+        return View["index.cshtml", model];
+      };
+      Get["/departments/new"] = _ => {
+        Dictionary<string, string> model = new Dictionary<string, string>{};
+        model.Add("form-type", "new-department");
+        return View["form.cshtml", model];
+      };
+      Post["/departments/new"] = _ => {
+        Dictionary<string, object> model = new Dictionary<string, object>{};
+        Department department = new Department(Request.Form["department-name"]);
+        department.Save();
+        model.Add("listDepartments", Department.GetAll());
+        model.Add("listCourses", Course.GetAll());
+        model.Add("listTeachers", Teacher.GetAll());
+        model.Add("listStudents", Student.GetAll());
+        model.Add("newDepartment", department);
+        model.Add("show-info", "new-department-info");
+
         return View["index.cshtml", model];
       };
     }
