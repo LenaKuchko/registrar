80cb052e53a1d2031ce1f81531a8a7a3d89a5c47
Marilyn Carlin and Olena Kuchko <marilyn+olena@>
2017-06-13 11:07:08 -0700

    add update method and test it

--- Objects/Department.cs
+++ Objects/Department.cs
@@ -31,6 +31,96 @@ namespace Registrar
       }
     }
 
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO departments (name) OUTPUT INSERTED.id VALUES (@DepartmentName);", conn);
+
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@DepartmentName";
+      nameParameter.Value = this.Name;
+
+      cmd.Parameters.Add(nameParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this.Id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static List<Department> GetAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM departments;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      List<Department> departments = new List<Department>{};
+      while (rdr.Read())
+      {
+        int departmentId = rdr.GetInt32(0);
+        string departmentName = rdr.GetString(1);
+        Department newDepartment = new Department(departmentName, departmentId);
+        departments.Add(newDepartment);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return departments;
+    }
+
+    public void UpdateDepartment(string newName)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("UPDATE departments SET name = @NewName OUTPUT INSERTED.name WHERE id = @DepartmentId;", conn);
+
+      SqlParameter newNameParameter = new SqlParameter();
+      newNameParameter.ParameterName = "@NewName";
+      newNameParameter.Value = newName;
+      cmd.Parameters.Add(newNameParameter);
+
+      SqlParameter departmentIdParameter = new SqlParameter();
+      departmentIdParameter.ParameterName = "@DepartmentId";
+      departmentIdParameter.Value = this.Id;
+      cmd.Parameters.Add(departmentIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this.Name = rdr.GetString(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+
     public static void DeleteAll()
     {
       SqlConnection conn = DB.Connection();
--- Tests/DepartmentTest.cs
+++ Tests/DepartmentTest.cs
@@ -14,14 +14,14 @@ namespace Registrar
     {
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=registrar_test;Integrated Security=SSPI;";
     }
-    // [Fact]
-    // public void Test_DepartmentDatabaseEmptyAtFirst()
-    // {
-    //   //Arrange, Act
-    //   int result = Department.GetAll().Count;
-    //   //Assert
-    //   Assert.Equal(0, result);
-    // }
+    [Fact]
+    public void Test_DepartmentDatabaseEmptyAtFirst()
+    {
+      //Arrange, Act
+      int result = Department.GetAll().Count;
+      //Assert
+      Assert.Equal(0, result);
+    }
 
     [Fact]
     public void Test_Equal_ReturnsTrueForSameInfo()
@@ -31,6 +31,33 @@ namespace Registrar
       Department secondDepartment = new Department("History");
       //Assert
       Assert.Equal(firstDepartment, secondDepartment);
+    }
+
+    [Fact]
+    public void Test_Save_AssignsIdToDepartmentInDatabase()
+    {
+      //Arrange
+      Department testDepartment = new Department("History");
+      testDepartment.Save();
+      //Act
+      Department savedDepartment = Department.GetAll()[0];
+      int testId = testDepartment.Id;
+      int expectedId = savedDepartment.Id;
+      //Assert
+      Assert.Equal(testId, expectedId);
+    }
+
+    [Fact]
+    public void Test_UpdateDepartment_ReturnsTrueIfDepartmentInfoIsTheSame()
+    {
+      //Arrange
+      Department firstTestDepartment = new Department("History");
+      firstTestDepartment.Save();
+      Department secondTestDepartment = new Department("Classics", firstTestDepartment.Id);
+      //Act
+      secondTestDepartment.UpdateDepartment("History");
+      //Assert
+      Assert.Equal(firstTestDepartment, secondTestDepartment);
     }
 
     public void Dispose()
